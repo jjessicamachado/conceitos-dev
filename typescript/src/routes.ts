c2da0ae7ff4f756d3ae1fc8913e0adae6de5f861
Jéssica Machado <jessicasmachado@live.com>
2020-08-29 18:05:15 -0300

    Evoluindo nas tipagens

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,14 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
-    return response.json({ message: "Hello World "});
+  const user = createUser({
+    email: "diego@rocketseat.com.br",
+    password: "1234",
+    techs: ["Node", "React", "Angular", { title: "JS", experience: 100 }],
+  });
+
+  console.log(user.email);
+
+  return response.json({ message: "Hello World " });
 }
